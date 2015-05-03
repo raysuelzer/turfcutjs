1f78411233a1a85fd5ecbd08bedcfca8aa23dba4
Raymond Suelzer <rsuelzer@gmail.com>
2015-05-02 20:20:43 -0400

    change self reference on addressmarker to an original object

--- src/AddressMarker.js
+++ src/AddressMarker.js
@@ -21,7 +21,7 @@ module.exports = function Marker(lat,lng, originalObject, opts) {
             let marker = new google.maps.Marker({ //this represents the actual google map marker object attched to the marker
                 position: new google.maps.LatLng(lat, lng),
                 map: map,
-                address: this //provide a reference to the parent marker,
+                originalObject: originalObject //provide a reference to the original object,
             });
 
         return marker;
